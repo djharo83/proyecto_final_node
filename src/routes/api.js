4565221d183b615d48c4fb0dc098fa54040d7383
Jose Redondo Reina <jredondopythonia@gmail.com>
2026-06-07 15:59:38 +0200

    Borrados archivos test y gitkeepers , incluido en peticionesUsers.Rest el link de render

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 
-// Nueva ruta de test
-router.use('/test', require('./api/test'));
-router.use('/users',require('./api/users'))
+
+
+router.use('/users',require('./api/users'));
 
 
 module.exports = router;
