fbafd2d13edde7f709a476532b1b8dffe356ade4
djharo83 <djharo83@gmail.com>
2026-06-09 18:40:16 +0200

    Modificacion de archivo swagger para ver si se entera vercel

--- src/config/swagger/swagger.js
+++ src/config/swagger/swagger.js
@@ -9,7 +9,7 @@ const SERVER_URL = IS_VERCEL
   ? `https://${process.env.VERCEL_URL || 'proyecto-final-node-five.vercel.app'}` 
   : `http://localhost:${process.env.PORT || 3000}`;
 
-// __dirname funciona en Vercel siempre que indiques el archivo en vercel.json
+//__dirname funciona en Vercel siempre que indiques el archivo en vercel.json
 const yamlPath = path.join(__dirname, './swagger.yaml');
 const swaggerSpec = yaml.parse(fs.readFileSync(yamlPath, 'utf8'));
 
