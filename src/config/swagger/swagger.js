f20e808cd1b7c299f9160facecd204e86a5ed77e
djharo83 <djharo83@gmail.com>
2026-06-13 14:08:18 +0200

    Configuracion swagger para servidor render v2

--- src/config/swagger/swagger.js
+++ src/config/swagger/swagger.js
@@ -1,23 +1,21 @@
-// src/config/swagger/swagger.js
 const yaml = require('yamljs');
 const path = require("path");
 const fs = require("fs");
 
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
-//__dirname funciona en Vercel siempre que indiques el archivo en vercel.json
 const yamlPath = path.join(__dirname, './swagger.yaml');
 const swaggerSpec = yaml.parse(fs.readFileSync(yamlPath, 'utf8'));
 
 
 const prodServer = {
-  // Toma la URL dinámica del Preview si existe, si no, usa la de producción por defecto
+  // Toma la URL dinámica del Preview si existe en render, si no, usa la de producción por defecto
   url: process.env.RENDER_EXTERNAL_URL || 'https://proyecto-final-node-js86.onrender.com',
   description: "Servidor de Producción  (Render)",
 };
 
 const localServer = {
-  url: `http://localhost:${process.env.PORT || 3000}`,
+  url: `http://localhost:3000`,
   description: "Servidor de Desarrollo (Local)",
 };
 
